eafe4ac19f8ef7c266599a825b06b37ca82ab5a6
BRCMOURA <brcmoura@gmail.com>
2024-10-04 21:09:38 -0300

    Add files via upload

---
+++ Exercício5.py
@@ -0,0 +1,8 @@
+#Resto da divisão
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+resto_divisao = (n1 % n2)
+
+print("O resto da divisão dos dois números é: ", resto_divisao)
